f62942f42d732a8455d647503743c0703b3f6d3d
Gauthier Viau <gauthierviau@gmail.com>
2013-10-13 23:45:00 -0400

    Added a class representing a Ray

---
+++ src/math/Ray.cpp
@@ -0,0 +1,72 @@
+#include "Ray.h"
+
+#include "Constants.h"
+#include "Plane.h"
+
+namespace Sketch3D
+{
+Ray::Ray(const Vector3& origin, const Vector3& direction) :
+    origin_(origin), direction_(direction)
+{
+    direction_.Normalize();
+}
+
+Ray::Ray(float ox, float oy, float oz, float dx, float dy, float dz) :
+    origin_(Vector3(ox, oy, oz)), direction_(Vector3(dx, dy, dz))
+{
+    direction_.Normalize();
+}
+
+bool Ray::IntersectsPlane(const Plane& plane, float* t) const
+{
+    // Are the direction vector and normal of the plane perpendicular?
+    float dot = direction_.Dot(plane.GetNormal());
+    if (dot <= EPSILON) {
+        return false;
+    }
+
+    // Get the distance if we asked it
+    if (t) {
+        *t = -(origin_.Dot(plane.GetNormal()) + plane.GetDistance()) / dot;
+    }
+
+    return true;
+}
+
+void Ray::SetOrigin(const Vector3& origin)
+{
+    origin_ = origin;
+}
+
+void Ray::SetOrigin(float ox, float oy, float oz)
+{
+    origin_ = Vector3(ox, oy, oz);
+}
+
+void Ray::SetDirection(const Vector3& direction)
+{
+    direction_ = direction.Normalized();
+}
+
+void Ray::SetDirection(float dx, float dy, float dz)
+{
+    direction_ = Vector3(dx, dy, dz);
+    direction_.Normalize();
+}
+
+void Ray::SetNormalizedDirection(const Vector3& direction)
+{
+    direction_ = direction;
+}
+
+const Vector3& Ray::GetOrigin() const
+{
+    return origin_;
+}
+
+const Vector3& Ray::GetDirection() const
+{
+    return direction_;
+}
+
+}
---
+++ src/math/Ray.h
@@ -0,0 +1,81 @@
+#ifndef SKETCH_3D_RAY_H
+#define SKETCH_3D_RAY_H
+
+#include "Vector3.h"
+
+namespace Sketch3D
+{
+
+// Forward class declaration
+class Plane;
+
+/**
+* @class Ray
+* Represents a ray in space as an origin and a unit length direction vector.
+*/
+class Ray
+{
+    public:
+        /**
+        * Constructor
+        * @param origin A 3D vector representing the origin of the ray
+        * @param direction A 3D vector representing the direction of the ray.
+        * It is not necessary for it to be normalized since the constructor
+        * will normalize it.
+        */
+                                Ray(const Vector3& origin,
+                                    const Vector3& direction);
+
+        /**
+        * Constructor. The direction will be normalized inside the constructor.
+        * @param ox The x position of the origin
+        * @param oy The y position of the origin
+        * @param oz The z position of the origin
+        * @param dx The x direction
+        * @param dy The y direction
+        * @param dz The z direction
+        */
+                                Ray(float ox, float oy, float oz,
+                                    float dx, float dy, float dz);
+
+        /**
+        * Determines if the ray intersects a Plane
+        * @param plane The plane to check for the intersection
+        * @param t A pointer to a float. If not NULL, then the float will have
+        * the distance from the origin of the ray at which the intersection
+        * occured.
+        * @return True if the ray intersected the plane, false otherwise.
+        */
+        bool                    IntersectsPlane(const Plane& plane,
+                                                float* t) const;
+        
+        inline void             SetOrigin(const Vector3& origin);
+        inline void             SetOrigin(float ox, float oy, float oz);
+
+        /**
+        * The direction will be normalized by the function
+        */
+        inline void             SetDirection(const Vector3& direction);
+
+        /**
+        * The direction will be normalized by the function
+        */
+        inline void             SetDirection(float dx, float dy, float dz);
+
+        /**
+        * Sets a new direction, but don't normalize it. It assumes it is
+        * normalized by the caller.
+        * @param direction The normalized vector to use as a new direction
+        */
+        inline void             SetNormalizedDirection(const Vector3& direction);
+
+        inline const Vector3&   GetOrigin() const;
+        inline const Vector3&   GetDirection() const;
+
+    private:
+        Vector3                 origin_;     /**< The origin of the ray */
+        Vector3                 direction_;  /**< The direction of the ray */
+};
+}
+
+#endif
