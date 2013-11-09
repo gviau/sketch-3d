846adff0a3b5ff85e944955a0e56fc256f2d4b83
Gauthier Viau <gauthierviau@gmail.com>
2013-11-09 12:41:10 -0500

    Added simple test

---
+++ tests/math/Ray.cpp
@@ -0,0 +1,17 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Plane.h"
+#include "Ray.h"
+
+using namespace Sketch3D;
+
+BOOST_AUTO_TEST_CASE(test_plane_intersection)
+{
+    Ray ray(Vector3(0.0f, 0.0f, 0.0f), Vector3(1.0f, 1.0f, 0.0f));
+    Plane plane(Vector3::UP, 10.0f);
+
+    float t;
+    bool val = ray.IntersectsPlane(plane, &t);
+
+    BOOST_CHECK(val == true);
+}
