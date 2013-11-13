f226306e8d921d05d6909c309a77e61b5f276506
Gauthier Viau <gauthierviau@gmail.com>
2013-11-12 19:05:58 -0500

    Added a define for inlining for gcc

--- src/common/Common.h
+++ src/common/Common.h
@@ -1,12 +1,16 @@
 #ifndef SKETCH_3D_COMMON_H
 #define SKETCH_3D_COMMON_H
 
+#include "../math/Constants.h"
+#include <cmath>
+
 namespace Sketch3D
 {
+// DEFINES
 #ifdef WIN32
-#define INLINE inline
+    #define INLINE inline
 #else
-#define INLINE
+    #define INLINE
 #endif
 }
 
