a038e02e41c7240389428317cc74ac521635bbe1
Gauthier Viau <gauthierviau@gmail.com>
2013-11-12 19:08:40 -0500

    Added some tests

---
+++ tests/math/Matrix3x3.cpp
@@ -0,0 +1,106 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Constants.h"
+#include "Matrix3x3.h"
+#include "Vector3.h"
+
+using namespace Sketch3D;
+
+BOOST_AUTO_TEST_CASE(test_transpose)
+{
+    Matrix3x3 m = Matrix3x3::IDENTITY;
+    BOOST_REQUIRE(m.Transpose() == m);
+
+    m(1, 0) = 5.0f;
+    m(2, 0) = 10.0f;
+
+    Matrix3x3 t(1.0f, 0.0f, 0.0f,
+                5.0f, 1.0f, 0.0f,
+                10.0f, 0.0f, 1.0f);
+    BOOST_REQUIRE(m.Transpose() == t);
+}
+
+BOOST_AUTO_TEST_CASE(test_rotation_around_x)
+{
+    Matrix3x3 m;
+    Vector3 v = Vector3::UP;
+    m.RotationAroundX(PI);
+    v *= m;
+    BOOST_REQUIRE(v == -Vector3::UP);
+
+    v = Vector3::ONE;
+    v *= m;
+    BOOST_REQUIRE(v == Vector3(1.0f, -1.0f, -1.0f));
+}
+
+BOOST_AUTO_TEST_CASE(test_rotation_around_y)
+{
+    Matrix3x3 m;
+    Vector3 v = Vector3::LOOK;
+    m.RotationAroundY(PI);
+    v *= m;
+    BOOST_REQUIRE(v == -Vector3::LOOK);
+
+    v = Vector3::ONE;
+    v *= m;
+    BOOST_REQUIRE(v == Vector3(-1.0f, 1.0f, -1.0f));
+}
+
+BOOST_AUTO_TEST_CASE(test_rotation_around_z)
+{
+    Matrix3x3 m;
+    Vector3 v = Vector3::RIGHT;
+    m.RotationAroundZ(PI);
+    v *= m;
+    BOOST_REQUIRE(v == -Vector3::RIGHT);
+
+    v = Vector3::ONE;
+    v *= m;
+    BOOST_REQUIRE(v == Vector3(-1.0f, -1.0f, 1.0f));
+}
+
+BOOST_AUTO_TEST_CASE(test_multiple_rotations)
+{
+    Matrix3x3 m, n, o;
+    m.RotationAroundX(PI_OVER_2);
+    n.RotationAroundY(40 * DEG_2_RAD);
+    o.RotationAroundZ(310 * DEG_2_RAD);
+
+    Matrix3x3 rot = m * n * o;
+
+    Vector3 v = Vector3::ONE;
+    Vector3 w = v * rot;
+
+    BOOST_REQUIRE(w == Vector3(-0.737208f, 0.686816f, -1.40883f));
+}
+
+BOOST_AUTO_TEST_CASE(test_rotate_around_axis)
+{
+}
+
+BOOST_AUTO_TEST_CASE(test_matrix_matrix_multiply)
+{
+    Matrix3x3 i = Matrix3x3::IDENTITY;
+    Matrix3x3 m(1.0f, 2.0f, 3.0f,
+                2.0f, 4.0f, 6.0f, 
+                3.0f, 6.0f, 9.0f);
+    BOOST_REQUIRE(i * m == m);
+    BOOST_REQUIRE(m * i == m);
+
+    Matrix3x3 n(9.0f, 6.0f, 3.0f,
+                6.0f, 4.0f, 2.0f,
+                3.0f, 2.0f, 1.0f);
+    Matrix3x3 o = m * n;
+    n *= m;
+    
+    Matrix3x3 r1(30.0f, 20.0f, 10.0f,
+                 60.0f, 40.0f, 20.0f,
+                 90.0f, 60.0f, 30.0f);
+    Matrix3x3 r2(30.0f, 60.0f, 90.0f,
+                 20.0f, 40.0f, 60.0f,
+                 10.0f, 20.0f, 30.0f);
+
+    BOOST_REQUIRE(o == r1);
+    BOOST_REQUIRE(n == r2);
+
+}   
---
+++ tests/math/Vector3.cpp
@@ -0,0 +1,75 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Constants.h"
+#include "Vector3.h"
+#include "Matrix3x3.h"
+
+using namespace Sketch3D;
+
+bool CompareTo(float a, float b)
+{
+    return fabs(a - b) <= EPSILON;
+}
+
+BOOST_AUTO_TEST_CASE(test_dot_product)
+{
+    Vector3 u(5.0f, 10.0f, 0.0f);
+    Vector3 v(1.0f, 1.0f, 1.0f);
+    float dot = u.Dot(v);
+    BOOST_REQUIRE(dot == 15.0f);
+}
+
+BOOST_AUTO_TEST_CASE(test_cross_product)
+{
+    Vector3 u = Vector3::RIGHT;
+    Vector3 v = Vector3::UP;
+    Vector3 w = u.Cross(v);
+
+    BOOST_REQUIRE(w == Vector3::LOOK);
+    BOOST_REQUIRE(v.Cross(u) != Vector3::LOOK);
+}
+
+BOOST_AUTO_TEST_CASE(test_length)
+{
+    Vector3 v(3.0f, 4.0f, 5.0f);
+    float expectedSqrLength = 3.0f * 3.0f + 4.0f * 4.0f + 5.0f * 5.0f;
+    float expectedLength = sqrtf(expectedSqrLength);
+    float sqrLength = v.SquaredLength();
+    float length = v.Length();
+
+    BOOST_REQUIRE(CompareTo(sqrLength, expectedSqrLength));
+    BOOST_REQUIRE(CompareTo(length, expectedLength));
+}
+
+BOOST_AUTO_TEST_CASE(test_normalize)
+{
+    Vector3 v(3.0f, 4.0f, 5.0f);
+    Vector3 u = v.Normalized();
+    v.Normalize();
+    Vector3 w(0.4242640687119285f,
+              0.565685424949238f,
+              0.7071067811865475f);
+    BOOST_REQUIRE(u == w);
+    BOOST_REQUIRE(v == w);
+}
+
+BOOST_AUTO_TEST_CASE(test_vector_matrix_multiply)
+{
+    Vector3 v = Vector3::ONE;
+    Matrix3x3 m = Matrix3x3::IDENTITY;
+
+    BOOST_REQUIRE(v * m == Vector3::ONE);
+
+    m(1, 0) = 5.0f;
+    m(2, 0) = 10.0f;
+
+    v *= m;
+
+    BOOST_REQUIRE(v == Vector3(16.0f, 1.0f, 1.0f));
+
+    m(1, 2) = 10.0f;
+
+    v *= m;
+
+    BOOST_REQUIRE(v == Vector3(31.0f, 1.0f, 11.0f));
+}
