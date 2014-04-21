3f0d9b621aab88a6ace8f4f9f262b27f8b25b617
gviau <gauthierviau@gmail.com>
2014-04-21 17:20:22 -0400

    Cleaned a little bit the directory

---
+++ include/common/Common.h
@@ -0,0 +1,17 @@
+#ifndef SKETCH_3D_COMMON_H
+#define SKETCH_3D_COMMON_H
+
+#include "../math/Constants.h"
+#include <cmath>
+
+namespace Sketch3D
+{
+// DEFINES
+#ifdef WIN32
+    #define INLINE inline
+#else
+    #define INLINE
+#endif
+}
+
+#endif
---
+++ include/render/ResourceManager.h
@@ -0,0 +1,18 @@
+#ifndef SKETCH_3D_RESOURCE_MANAGER_H
+#define SKETCH_3D_RESOURCE_MANAGER_H
+
+namespace Sketch3D {
+
+/**
+ * @class ResourceManager
+ * This class allow the user to laod a resource once and to cache it, so that
+ * it can be reused quickly without loading it a second time.
+ */
+class ResourceManager {
+	public:
+	private:
+};
+
+}
+
+#endif
