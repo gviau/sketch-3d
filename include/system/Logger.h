a204e0d7e8f35b728809b1d697da08bf1e037c43
gviau <gauthierviau@gmail.com>
2014-10-16 22:31:59 -0400

    Implemented logging level to filter out unecessary log messages

--- include/system/Logger.h
+++ include/system/Logger.h
@@ -9,6 +9,20 @@ using namespace std;
 namespace Sketch3D {
 
 /**
+ * @enum LoggerLevel_t
+ * Determines what information should the logger output. When the Logger has one of those value
+ * set, it will only output this level and all the one above. This means that when set to
+ * LOGGER_LEVEL_ERROR, by example, it will only output error messages but if it set to
+ * LOGGER_LEVEL_INFO (default), it will ouput info messages, warnings and errors.
+ */
+enum LoggerLevel_t {
+    LOGGER_LEVEL_ERROR,
+    LOGGER_LEVEL_WARNING,
+    LOGGER_LEVEL_INFO,
+    LOGGER_LEVEL_DEBUG
+};
+
+/**
  * @class Logger
  * This class is a singleton and is used to log information, warnings and
  * errors into a structure html file.
@@ -21,6 +35,12 @@ class Logger {
 					   ~Logger();
 
 		static Logger*	GetInstance();
+
+        /**
+         * Set logger level.
+         * @param level The new logger level to use
+         */
+        void            SetLoggerLevel(LoggerLevel_t level);
 
 		/**
 		 * Write a debug message in the log
@@ -50,6 +70,7 @@ class Logger {
 		static Logger	instance_;	/**< Singleton's instance */
 
 		ofstream		file_;		/**< The file to which we're writting */
+        LoggerLevel_t   level_;     /**< Logger output level */
 
 		/**
 		 * Constructor
--- src/system/Logger.cpp
+++ src/system/Logger.cpp
@@ -8,7 +8,7 @@ namespace Sketch3D {
 
 Logger Logger::instance_;
 
-Logger::Logger() {
+Logger::Logger() : level_(LOGGER_LEVEL_INFO) {
 	file_.open("Log.html");
 	file_ << "<html><head><style type=\"text/css\">p {margin:3}</style>" <<
 			 "</head><body bgcolor=\"#8a8a8a\">" << endl;
@@ -26,29 +26,41 @@ Logger* Logger::GetInstance() {
 	return &instance_;
 }
 
+void Logger::SetLoggerLevel(LoggerLevel_t level) {
+    level_ = level;
+}
+
 void Logger::Debug(const string& message) {
-	file_ << "<p style=\"background-color:#aabbcc\">[DEBUG] ";
-	file_ << GetCurrentTime() << " - ";
-	file_ << message;
-	file_ << "</p>" << endl;
+    if (level_ >= LOGGER_LEVEL_DEBUG) {
+	    file_ << "<p style=\"background-color:#aabbcc\">[DEBUG] ";
+	    file_ << GetCurrentTime() << " - ";
+	    file_ << message;
+	    file_ << "</p>" << endl;
+    }
 }
 
 void Logger::Info(const string& message) {
-	file_ << "<p>" << GetCurrentTime() << " - " << message << "</p>" << endl;
+    if (level_ >= LOGGER_LEVEL_INFO) {
+	    file_ << "<p>" << GetCurrentTime() << " - " << message << "</p>" << endl;
+    }
 }
 
 void Logger::Warning(const string& message) {
-	file_ << "<p style=\"background-color:#ffbb12\">[WARNING] ";
-	file_ << GetCurrentTime() << " - ";
-	file_ << message;
-	file_ << "</p>" << endl;
+    if (level_ >= LOGGER_LEVEL_WARNING) {
+	    file_ << "<p style=\"background-color:#ffbb12\">[WARNING] ";
+	    file_ << GetCurrentTime() << " - ";
+	    file_ << message;
+	    file_ << "</p>" << endl;
+    }
 }
 
 void Logger::Error(const string& message) {
-	file_ << "<p style=\"background-color:#ff0012\">[ERROR] ";
-	file_ << GetCurrentTime() << " - ";
-	file_ << message;
-	file_ << "</p>" << endl;
+    if (level_ >= LOGGER_LEVEL_ERROR) {
+	    file_ << "<p style=\"background-color:#ff0012\">[ERROR] ";
+	    file_ << GetCurrentTime() << " - ";
+	    file_ << message;
+	    file_ << "</p>" << endl;
+    }
 }
 
 string Logger::GetCurrentTime() const {
