f33efc995cf17c9aa728a7bd9fa1f65707b33ed7
gviau <gauthierviau@gmail.com>
2015-04-22 12:52:24 -0400

    Added an utility function which takes a surface and pack the vertices in an interleaved array

--- sketch3d-main/include/render/BufferObject.h
+++ sketch3d-main/include/render/BufferObject.h
@@ -11,6 +11,17 @@ namespace Sketch3D {
 
 // Forward declaration
 class Matrix4x4;
+struct SurfaceTriangles_t;
+
+/**
+ * @enum BufferObjectError_t
+ * Describes an error code that can be returned by the {Set,Append}{Vertex,Index}Data functions
+ */
+enum BufferObjectError_t {
+    BUFFER_OBJECT_ERROR_NONE,
+    BUFFER_OBJECT_ERROR_INVALID_VERTEX_ATTRIBUTES,
+    BUFFER_OBJECT_ERROR_NOT_ENOUGH_SPACE
+};
 
 /**
  * @enum BufferUsage_t
@@ -53,71 +64,94 @@ class SKETCH_3D_API BufferObject {
          * @param vertexAttributes The vertex attributes to use with this buffer object
          * @param usage Usage of the buffer
          */
-                                BufferObject(const VertexAttributesMap_t& vertexAttributes, BufferUsage_t usage=BUFFER_USAGE_STATIC);
+                                    BufferObject(const VertexAttributesMap_t& vertexAttributes, BufferUsage_t usage=BUFFER_USAGE_STATIC);
 
         /**
          * Destructor
          */
-        virtual                ~BufferObject();
+        virtual                    ~BufferObject();
 
         /**
          * Actually render the buffer object contents
          */
-        virtual void            Render() = 0;
+        virtual void                Render() = 0;
 
         /**
          * Actually render several instances of the buffer object contents
          * @param modelMatrices A list of model matrices to use to draw all the instances of the buffer object. The
          * size of this list is the number of instances that will be drawn
          */
-        virtual void            RenderInstances(const vector<Matrix4x4>& modelMatrices) = 0;
+        virtual void                RenderInstances(const vector<Matrix4x4>& modelMatrices) = 0;
 
         /**
          * Set the vertices for the vertex buffer
          * @param vertexData An array of float that represent the vertex data
          * @param presentVertexAttributes Bitfield specifying what vertex attributes are actually present
-         * @return false if the vertex attributes specified on construction are not all present, true otherwise
+         * @return An error code from the BufferObjectError_t enum
          */
-        virtual bool            SetVertexData(const vector<float>& vertexData, int presentVertexAttributes) = 0;
+        virtual BufferObjectError_t SetVertexData(const vector<float>& vertexData, int presentVertexAttributes) = 0;
 
         /**
          * Append vertices data at the end of the vertex buffer
          * @param vertexData An array of float that represent the vertex data to append
          * @param presentVertexAttributes Bitfield specifying what vertex attributes are actually present
-         * @return false if the vertex attributes specified on construction are not all present, true otherwise
+         * @return An error code from the BufferObjectError_t enum
          */
-        virtual bool            AppendVertexData(const vector<float>& vertexData, int presentVertexAttributes) = 0;
+        virtual BufferObjectError_t AppendVertexData(const vector<float>& vertexData, int presentVertexAttributes) = 0;
 
         /**
          * Set the indices for the index buffer
          * @param indexData An array of unsigned short that represent the index data
          * @param numIndex The number of index in the array
+         * @return An error code from the BufferObjectError_t enum
          */
-        virtual void            SetIndexData(unsigned short* indexData, size_t numIndex) = 0;
+        virtual BufferObjectError_t SetIndexData(unsigned short* indexData, size_t numIndex) = 0;
 
         /**
          * Append indices data at the end of the index buffer
          * @param indexData an array of unsigned short that represent the index data to append
          * @param numIndex The number of index in the array to append
+         * @return An error code from the BufferObjectError_t enum
          */
-        virtual void            AppendIndexData(unsigned short* indexData, size_t numIndex) = 0;
+        virtual BufferObjectError_t AppendIndexData(unsigned short* indexData, size_t numIndex) = 0;
 
         /**
          * Prepare buffers for instanced rendering
          */
         virtual void            PrepareInstanceBuffers() = 0;
 
+        size_t                  GetVertexAttributesBitField() const;
         size_t                  GetId() const;
 
     protected:
         VertexAttributesMap_t   vertexAttributes_;  /**< The vertex attributes to use for the vertex buffer */
         BufferUsage_t           usage_;       /**< The buffer usage */
         size_t                  vertexCount_;
+        size_t                  stride_;
         size_t                  indexCount_;
         size_t                  id_;
 
         static size_t           nextAvailableId_;
+
+        /**
+         * Checks if the present vertex attributes in the vertex data presented to this buffer
+         * are the same as the vertex attributes used when creating the buffer
+         * @param presentVertexAttributes A bit field describing the present vertex attributes
+         * @return true if the present vertex attributes are all the same as the one from the buffer, false otherwise
+         */
+        bool                    AreVertexAttributesValid(int presentVertexAttributes) const;
 };
+
+/**
+ * Pack a surface vertex data into an interleaved array of float for a non-skinned mesh DON'T USE ON SKINNED MESH, YOU'LL LOSE INFORMATION
+ * @param surface The surface from which to get the vertex data
+ * @param attributesFromIndex A map describing where in the array to place the corresponding vertex data
+ * @param vertexData The resulting interleaved array
+ * @param presentVertexAttributes A bit field describing which vertex attributes are present in the vertex data
+ * @param stride The size of a single vertex in bytes
+ */
+void PackSurfaceTriangleVertices(const SurfaceTriangles_t* surface, const map<size_t, VertexAttributes_t>& attributesFromIndex,
+                                 vector<float>& vertexData, int& presentVertexAttributes, size_t& stride);
 
 }
 
--- sketch3d-main/src/render/BufferObject.cpp
+++ sketch3d-main/src/render/BufferObject.cpp
@@ -1,11 +1,13 @@
 #include "render/BufferObject.h"
 
+#include "render/Mesh.h"
+
 namespace Sketch3D {
 
 size_t BufferObject::nextAvailableId_ = 0;
 
 BufferObject::BufferObject(const VertexAttributesMap_t& vertexAttributes, BufferUsage_t usage) : vertexAttributes_(vertexAttributes), usage_(usage),
-        vertexCount_(0), indexCount_(0)
+        vertexCount_(0), stride_(0), indexCount_(0)
 {
     id_ = nextAvailableId_++;
 }
@@ -13,7 +15,100 @@ BufferObject::BufferObject(const VertexAttributesMap_t& vertexAttributes, Buffer
 BufferObject::~BufferObject() {
 }
 
+size_t BufferObject::GetVertexAttributesBitField() const {
+    size_t vertexAttributes = 0;
+    VertexAttributesMap_t::const_iterator it = vertexAttributes_.begin();
+    for (; it != vertexAttributes_.end(); ++it) {
+        vertexAttributes |= it->first;
+    }
+
+    return vertexAttributes;
+}
+
 size_t BufferObject::GetId() const {
     return id_;
 }
+
+bool BufferObject::AreVertexAttributesValid(int presentVertexAttributes) const {
+    // We implicitely count position
+    size_t count = 1;
+    VertexAttributesMap_t::const_iterator it = vertexAttributes_.begin();
+
+    for (; it != vertexAttributes_.end(); ++it) {
+        if ((presentVertexAttributes & it->first) > 0) {
+            count += 1;
+        }
+    }
+
+    return count == vertexAttributes_.size();
+}
+
+void PackSurfaceTriangleVertices(const SurfaceTriangles_t* surface, const map<size_t, VertexAttributes_t>& attributesFromIndex,
+                                     vector<float>& vertexData, int& presentVertexAttributes, size_t& stride)
+{
+	// Interleave the data
+    vertexData.reserve(surface->numVertices * 3 +
+                       surface->numNormals * 3 +
+                       surface->numTexCoords * 2 +
+                       surface->numTangents * 3);
+
+    bool hasNormals = surface->numNormals > 0;
+    bool hasTexCoords = surface->numTexCoords > 0;
+    bool hasTangents = surface->numTangents > 0;
+    bool hasBones = surface->numBones > 0;
+    bool hasWeights = surface->numWeights > 0;
+
+    Vector3 vertex;
+    for (size_t j = 0; j < surface->numVertices; j++) {
+        map<size_t, VertexAttributes_t>::const_iterator v_it = attributesFromIndex.begin();
+
+        for (; v_it != attributesFromIndex.end(); ++v_it) {
+            switch (v_it->second) {
+                case VERTEX_ATTRIBUTES_POSITION:
+                    vertex = surface->vertices[j];
+                    vertexData.push_back(vertex.x); vertexData.push_back(vertex.y); vertexData.push_back(vertex.z);
+                    break;
+
+                case VERTEX_ATTRIBUTES_NORMAL:
+                    if (hasNormals) {
+                        Vector3 normal = surface->normals[j];
+                        vertexData.push_back(normal.x); vertexData.push_back(normal.y); vertexData.push_back(normal.z);
+                    }
+                    break;
+
+                case VERTEX_ATTRIBUTES_TEX_COORDS:
+                    if (hasTexCoords) {
+                        Vector2 texCoords = surface->texCoords[j];
+                        vertexData.push_back(texCoords.x); vertexData.push_back(texCoords.y);
+                    }
+                    break;
+
+                case VERTEX_ATTRIBUTES_TANGENT:
+                    if (hasTangents) {
+                        Vector3 tangents = surface->tangents[j];
+                        vertexData.push_back(tangents.x); vertexData.push_back(tangents.y); vertexData.push_back(tangents.z);
+                    }
+                    break;
+            }
+        }
+	}
+
+    stride = sizeof(float) * 3;
+    presentVertexAttributes = 0;
+    if (hasNormals) {
+        presentVertexAttributes |= VERTEX_ATTRIBUTES_NORMAL;
+        stride += sizeof(float) * 3;
+    }
+
+    if (hasTexCoords) {
+        presentVertexAttributes |= VERTEX_ATTRIBUTES_TEX_COORDS;
+        stride += sizeof(float) * 2;
+    }
+
+    if (hasTangents) {
+        presentVertexAttributes |= VERTEX_ATTRIBUTES_TANGENT;
+        stride += sizeof(float) * 3;
+    }
+}
+
 }
