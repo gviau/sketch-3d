e0fec1b4cd5f24fa20be4a57a93f2a75a28ac48a
gviau <gauthierviau@gmail.com>
2015-08-27 23:17:06 -0400

    Base OpenGL test

--- samples/InteractiveWater/src/Main.cpp
+++ samples/InteractiveWater/src/Main.cpp
@@ -1,4 +1,7 @@
+#include <math/Vector4.h>
+
 #include <render/RenderContext.h>
+#include <render/RenderDevice.h>
 
 #include <system/Window.h>
 #include <system/WindowEvent.h>
@@ -26,6 +29,21 @@ int main(int argc, char** argv) {
 
     shared_ptr<RenderContext> renderContext = CreateRenderContext(RenderSystem_t::OPENGL);
     bool init = renderContext->Initialize(window, renderParameters);
+
+    shared_ptr<RenderDevice> renderDevice = CreateRenderDevice(RenderSystem_t::OPENGL);
+    init = renderDevice->Initialize(renderContext);
+
+    while (window.IsOpen())
+    {
+        WindowEvent event;
+        if (window.PollEvents(event))
+        {
+        }
+
+        renderDevice->ClearRenderTargets(Vector4(0.1f, 0.1f, 0.1f, 1.0f));
+
+        renderContext->SwapBuffers();
+    }
 
     return 0;
 }
--- sketch3d-main/src/render/RenderDevice.cpp
+++ sketch3d-main/src/render/RenderDevice.cpp
@@ -1,5 +1,7 @@
 #include "render/RenderDevice.h"
 
+#include "render/OpenGL/RenderDeviceOpenGL.h"
+
 #if PLATFORM == PLATFORM_WIN32
 #include "render/Direct3D11/RenderDeviceDirect3D11.h"
 #endif
@@ -17,10 +19,10 @@ shared_ptr<RenderDevice> SKETCH_3D_API CreateRenderDevice(RenderSystem_t renderS
 #if PLATFORM == PLATFORM_WIN32
     switch (renderSystem) {
         case RenderSystem_t::DIRECT3D11:    renderDevice = new RenderDeviceDirect3D11; break;
-        case RenderSystem_t::OPENGL:        renderDevice = nullptr; break;
+        case RenderSystem_t::OPENGL:        renderDevice = new RenderDeviceOpenGL; break;
     }
 #elif PLATFORM == PLATFORM_LINUX
-    renderDevice = nullptr;
+    renderDevice = new RenderDeviceOpenGL;
 #endif
 
     if (renderDevice == nullptr) {
