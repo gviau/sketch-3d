c7ed525d5d17136e40ee2fef55aaa8de2716dfe4
gviau <gauthierviau@gmail.com>
2015-08-29 12:51:37 -0400

    Added a class that automatically transposes a matrix when using it in a constant buffer to avoid programmer errors

--- sketch3d-main/include/render/ConstantBuffers.h
+++ sketch3d-main/include/render/ConstantBuffers.h
@@ -7,24 +7,39 @@
 #include "math/Vector4.h"
 
 namespace Sketch3D {
+
+/**
+ * @class GPUMatrix4x4
+ * This class is a class that takes a 4x4 matrix as input and automatically transpose it, to avoid programmer errors
+ */
+class GPUMatrix4x4
+{
+    public:
+        GPUMatrix4x4(const Matrix4x4& src) : data_(src.Transpose()) {}
+        void operator= (const Matrix4x4& rhs) { data_ = rhs.Transpose(); }
+
+    private:
+        Matrix4x4 data_;
+};
+
 // Constant buffers should be aligned to 16 bytes boundaries for performance reason
 struct ALIGNED_(16) PassConstants_t {
-    Matrix4x4 projectionMatrix;
-    Matrix4x4 viewMatrix;
+    GPUMatrix4x4 projectionMatrix;
+    GPUMatrix4x4 viewMatrix;
 };
 
 struct ALIGNED_(16) DrawConstants_t {
-    Matrix4x4 modelMatrix;
-    Matrix4x4 modelViewProjectionMatrix;
-    Matrix4x4 ViewProjectionMatrix;
-    Matrix4x4 modelViewMatrix;
+    GPUMatrix4x4 modelMatrix;
+    GPUMatrix4x4 modelViewProjectionMatrix;
+    GPUMatrix4x4 ViewProjectionMatrix;
+    GPUMatrix4x4 modelViewMatrix;
 
-    Matrix4x4 inverseTransposeModelViewMatrix;
+    GPUMatrix4x4 inverseTransposeModelViewMatrix;
 };
 
 struct ALIGNED_(16) ShadowConstants_t {
-    Matrix4x4 lightViewMatrix;
-    Matrix4x4 lightProjectionMatrix;
+    GPUMatrix4x4 lightViewMatrix;
+    GPUMatrix4x4 lightProjectionMatrix;
 };
 
 }
